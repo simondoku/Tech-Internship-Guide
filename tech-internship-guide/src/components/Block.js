90f11b760d1b3dff3840f47485321c124ca86caf
Tella Adegbola <92543868+adegbola08@users.noreply.github.com>
2023-09-15 00:43:29 +0000

    block icons added

--- tech-internship-guide/src/components/Block.js
+++ tech-internship-guide/src/components/Block.js
@@ -1,16 +1,38 @@
 import React from "react";
 import Card from 'react-bootstrap/Card';
+import RemoveRedEyeOutlinedIcon from '@mui/icons-material/RemoveRedEyeOutlined';
+import LaunchIcon from '@mui/icons-material/Launch';
+import { Button } from "react-bootstrap";
+
+const IconButtonWrapper = (props) => {
+    const { children } = props;
+    return(
+        <Button variant="dark" size="sm">
+            {children}
+        </Button>
+    ); 
+};
 
 export const Block = (props) => {
     const {blockName, blockLink} = props;
 
     return(
         <Card style={{ width: '18rem', backgroundColor: 'black', color: 'white' }} className="mb-2 text-light">
-            <div style={{backgroundColor: "#eeeeee", margin: '10px', height: '10rem'}} className="rounded">
+            <div style={{backgroundColor: "#eeeeee", margin: '10px', height: '9rem'}} className="rounded">
                 <Card.Img className="img-fluid" variant="top" src=""/>
             </div>
-            <div style={{display: 'flex'}} className="pb-1 px-2">
-                {blockName} {blockLink}
+            <div style={{display: 'flex', flex: '1', justifyContent: 'space-between'}} className="pb-1 px-2">
+                <div>
+                    {blockName} {blockLink}
+                </div>
+                <div>
+                    <IconButtonWrapper>
+                        <RemoveRedEyeOutlinedIcon fontSize="small"/>
+                    </IconButtonWrapper>
+                    <IconButtonWrapper>
+                        <LaunchIcon fontSize="small"/>  
+                    </IconButtonWrapper>
+                </div>
             </div>          
         </Card>
     );
