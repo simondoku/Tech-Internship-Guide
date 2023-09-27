345d4dd48cf55aa40ab85d27d95ead04bbdd9ca6
Tella Adegbola <92543868+adegbola08@users.noreply.github.com>
2023-09-27 04:09:02 +0000

    add a site button

--- tech-internship-guide/src/components/Navbar.js
+++ tech-internship-guide/src/components/Navbar.js
@@ -7,10 +7,16 @@ export const Navbar = () => {
         <div className="px-3">
             <NavbarBoot data-bs-theme="dark">
                 <div style={{display: 'flex', flex: '1'}}>
-                    <NavbarBoot.Brand href="#home" className="text-primary">Tech Internship Guide</NavbarBoot.Brand>
+                    <NavbarBoot.Brand href="#home" className="text-primary">
+                        <img src="" width="25" height="25" className="d-inline-block align-top me-1" alt=""/>
+                        Tech Internship Guide
+                    </NavbarBoot.Brand>
                     <NavbarBoot.Toggle />
                     <NavbarBoot.Collapse className="justify-content-end">
-                        <Button variant="info" size="sm" className="rounded-3 me-2">Add a Site</Button>
+                        <a href="https://docs.google.com/forms/d/e/1FAIpQLScbqk5Y9MEuzUoc6jsXUTVzWzs8bEEUhPertu95d1Rau4i1Zw/viewform" 
+                        target="_blank" rel="noreferrer">
+                            <Button variant="info" size="sm" className="rounded-3 me-2">Add a Site</Button>
+                        </a>
                         <Button variant="primary" size="sm" className="rounded-pill">This site updates every 30th</Button>
                     </NavbarBoot.Collapse>
                 </div>
