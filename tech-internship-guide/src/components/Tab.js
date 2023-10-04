4f93c212fa8282d4c79085e9ca895e565cbbb313
Tella Adegbola <92543868+adegbola08@users.noreply.github.com>
2023-10-04 04:07:12 +0000

    tab styles

--- tech-internship-guide/src/components/Tab.js
+++ tech-internship-guide/src/components/Tab.js
@@ -9,7 +9,7 @@ const NavItem = (props) => {
   const {tabName, eventKey} = props;
   return(
     <Nav.Item>
-      <Nav.Link className="text-light border rounded-3 mb-3 p-1 px-2" eventKey={eventKey}>{tabName}</Nav.Link>
+      <Nav.Link className="text-light mb-3 rounded-4 px-2" style={{border: '1px solid #9C9B9B',}} eventKey={eventKey}>{tabName}</Nav.Link>
     </Nav.Item>
   );
 };
@@ -24,16 +24,16 @@ const TabPane = (props) => {
 export const TabGroup = () => {
     return(
         <Tab.Container id="left-tabs" defaultActiveKey="classes">
-          <Row className="ps-3">
-            <Col sm={2}>
-              <Nav variant="pills" className="flex-column bg-dark rounded-3 p-2 mt-5 pt-3" style={{border: '1px solid #9C9B9B'}}>
+          <Row>
+            <Col sm={3} style={{display: 'flex', justifyContent: 'center'}}>
+              <Nav variant="pills" className="flex-column bg-dark rounded-4 p-4 mt-sm-0 mt-4 mb-5" style={{border: '1px solid #9C9B9B', width: '72%',  justifyContent: 'space-between'}}>
               {TAB_NAMES.map((item, index) => (
                 <NavItem key={index} tabName={item?.tabName} eventKey={item?.eventKey}/>
               ))}
               </Nav>
             </Col>
-            <Col sm={10}>
-              <div style={{overflow: 'auto', height: '70vh'}}>
+            <Col sm={9}>
+              <div style={{overflow: 'auto', height: '68vh'}}>
                 <Tab.Content >
                   {TAB_NAMES.map((item, index) => (
                     <TabPane key={index} eventKey={item?.eventKey} content={item?.comp}/>  
