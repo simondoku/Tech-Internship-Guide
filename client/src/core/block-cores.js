939b23c527e59bcdbbf11ab6a8ff25bb5d1939aa
Simon Doku <95186419+simondoku@users.noreply.github.com>
2023-10-09 09:06:11 +0000

    added images

--- client/src/core/block-cores.js
+++ client/src/core/block-cores.js
@@ -1,19 +1,17 @@
 export const BLOCK_SECTIONS = {
     pFundamentals: ['w3schools', 'freeCodeCamp', 'codeacademy', 'neetCode', 'coursera', 'geeksforgeeks', "harvardCS50"],
     dSA: ['neetCode', 'coursera', 'geeksforgeeks'],
-    note: ['personalNote', 'onlineNotes'],
-    webDev: ['githubAWDRepo', 'mdn','w3schools','webDevByGoogle','frontEndMasters'],
-    gameDev: ['unityLearn','unrealEngine','gameDevNet','coursera'],
-    resBuild: ['samplRes','harvardWords','balanceCareers','resumeGen','zety','resumeCom','monsterResume'],
-    resTemp: ['canvaTem','microsoftCanva','googleDocsTemp','template','hloom'],
-    portShow: ['githubPage','dribble','behance','carbonMade'],
-    techInterview: ['hackerRank','leetcode','geeksforgeeks','interviewBit','pramp'],
-    behInterview: ['interviewio','codeSignal','gainlo','pramp','behavioralInterview','theMuse'],
-    intern: ['gitHubIntern','gitHubNewGrad','zipRecruiter','glassDoor','simplyHired','reu'],
-    diverseGroup: ['colorstack','rewritingTheCode','nsbe'],
-    techEvents: ['ghc','afroTech','ces','techCrunch','webSummit','googleio','microIgnite','aws','rsa','blackhat','defcon','apple'],
-    profOrg: ['codepath','acm','compTIA'],
-
+    note: ['personalNote', 'onlineNote'],
 } 
 
-export const BLOCK_IMAGES = new Set([ 'w3schools', 'codeacademy', 'freeCodeCamp', 'neetCode' ]);
+export const BLOCK_IMAGES = new Set([ 
+    'acm','afroTech', 'algoExpert','anitaB','applewwdc','awsReinvent','behance','blackHat','canvaTemp','carbonMade','cargo',
+    'ces','codeacedemy','codepath','codepen','codeSignal','colorstack','compTIA','coursera','defcon', 'dribbble','edX',
+    'freeCodeCamp', 'frontEndMasters','gainlo','gameDevnet','geeksforgeeks','ghc','github','gitHubIntern','githubPages','glassDoor',
+    'google','googleDocsTemps','googleio','hackerRank','harvardCS50','hloom','indeed','interviewBit','interviewio','jsfiddle',
+    'khanAcademy','leetcode','mdn','microsoftIgnite','microsoftWord','mit','monsterResume','neetCode','nsbe','onlineNotes','pramp',
+    'resumeGen','resumeCom','reu','rewritingTheCode','rsa','simplyHired','techCrunchDisrupt','template','theBalanceCareers','theMuse',
+    'udacity','udemy','unity','unrealEngine','w3schools','webDevByGoogle','webSummit','wix','zety','zipRecruiter'
+
+
+]);
