6a5219291c15c004fe20106c4591249e8d1f45a5
beverleyherne <bherne@stetson.edu>
2023-10-09 14:37:46 -0400

    updated content

--- client/src/tabs/Networking.js
+++ client/src/tabs/Networking.js
@@ -5,7 +5,7 @@ export const Networking = () => {
     return(
         <div className="text-light">
             <BlockSection sectionName={"Diversity Groups"} sectionId={'diverseGroup'}/>
-            <BlockSection sectionName={'Tech Events and Meetups'} sectionId={'techEvents'}/>
+            <BlockSection sectionName={'Tech Conferences and Events'} sectionId={'techEvents'}/>
             <BlockSection sectionName={'Professional Organizations'} sectionId={'profOrg'}/>
         </div>
     );
