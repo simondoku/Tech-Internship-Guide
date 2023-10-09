f3fd981064e05f824904075b4fcfd699d4ec4b2e
Tella Adegbola <92543868+adegbola08@users.noreply.github.com>
2023-10-09 21:22:23 +0000

    fixed error

--- client/src/core/block-cores.js
+++ client/src/core/block-cores.js
@@ -12,12 +12,12 @@ export const BLOCK_SECTIONS = {
     behInterview: ['interviewio','codeSignal','gainlo','pramp','indeed','theMuse'],
     intern: ['gitHubIntern','zipRecruiter','glassDoor','simplyHired','reu'],
     diverseGroup: ['colorstack','rewritingTheCode','nsbe'],
-    techEvents: ['ghc','afroTech','ces','techCrunch','webSummit','googleio','microIgnite','aws','rsa','blackhat','defcon','apple'],
+    techEvents: ['ghc','afroTech','ces','techCrunch','webSummit','googleio','microIgnite','aws','rsa','blackHat','defcon','apple'],
     profOrg: ['codepath','acm','compTIA'],
 } 
 
 export const BLOCK_IMAGES = new Set([ 
-    'w3schools', 'codeacademy', 'freeCodeCamp', 'neetCode', 'acm','afroTech', 'algoExpert','anitaB','apple','aws','behance','blackhat','theBalanceCareers','canvaTem','carbonMade','cargo',
+    'w3schools', 'codeacademy', 'freeCodeCamp', 'neetCode', 'acm','afroTech', 'algoExpert','anitaB','apple','aws','behance','blackHat','theBalanceCareers','canvaTem','carbonMade','cargo',
     'ces','codeacedemy','codepath','codepen','codeSignal','colorstack','compTIA','coursera','defcon', 'dribble','edX',
     'freeCodeCamp', 'frontEndMasters','gainlo','gameDevNet','geeksforgeeks','ghc','github','githubAWDRepo','gitHubIntern','githubPage','glassDoor',
     'google','googleDocsTemp','googleio','hackerRank','harvardCS50','hloom','indeed','interviewBit','interviewio','jsfiddle',
